72ed2d16b1a29672af60f129b0cda95de151973c
Jyjar <156258612+Jyjar@users.noreply.github.com>
2024-11-20 09:12:42 +0100

    Updated to use express.

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const express = require("express");
+const path = require("path");
+const app = express();
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname + ".html";
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-        fs.readFile('./404.html', function(error404, data404) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write(data404);
-            return res.end();
-        });
-        return;
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
